3c73ab265bb1abdeb4571d9938d5b0e402049dc4
Vincent Stumpf <vincents.995@gmail.com>
2022-11-11 21:33:29 -0800

    don't check out files, already done

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,9 @@
 
 set -eu
 
-REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
-
-echo "## Initializing git repo..."
-git init
-echo "### Adding git remote..."
-git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
-echo "### Getting branch"
-BRANCH=${GITHUB_REF#*refs/heads/}
-echo "### git fetch $BRANCH ..."
-git fetch origin $BRANCH
-echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
-git checkout $BRANCH
-
 echo "## Configuring git author..."
-git config --global user.email "clang-format@1337z.ninja"
-git config --global user.name "Clang Format"
+git config --global user.email "clang-format@rathena.com"
+git config --global user.name "rAthena Clang Format"
 
 # Ignore workflow files (we may not touch them)
 git update-index --assume-unchanged .github/workflows/*
