5be737a988d49ddf42d54c90e5e26f2e67694f44
Vincent Stumpf <vincents.995@gmail.com>
2022-11-11 21:43:53 -0800

    clean up more stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
+echo "## Marking pwd as safe"
+git config --global --add safe.directory "$PWD"
+
 echo "## Configuring git author..."
 git config --global user.email "clang-format@rathena.com"
 git config --global user.name "rAthena Clang Format"
-
-# Ignore workflow files (we may not touch them)
-git update-index --assume-unchanged .github/workflows/*
 
 echo "## Running clang-format on C/C++ source in src/ directory"
 SRC=$(git ls-tree -r HEAD src | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2)
