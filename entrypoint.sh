51e3ef3b446fb485f1b615ac6491063f65c2e9b1
Vincent Stumpf <vincents.995@gmail.com>
2022-11-11 21:58:55 -0800

    update ubuntu/clang-format version

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,8 @@ git config --global user.name "rAthena Clang Format"
 echo "## Running clang-format on C/C++ source in src/ directory"
 SRC=$(git ls-tree -r HEAD src | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2)
 
+echo "## Using $(clang-format --version)" 
+
 clang-format -style=file -i $SRC
 
 echo "## Commiting files..."
