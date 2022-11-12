e1702a69f2a11a7fe07b03ab6455388432cba847
Vincent Stumpf <vincents.995@gmail.com>
2022-11-12 00:56:04 -0800

    update some stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,13 @@ echo "## Configuring git author..."
 git config --global user.email "clang-format@rathena.com"
 git config --global user.name "rAthena Clang Format"
 
-echo "## Using $(clang-format --version)" 
-
 echo "## Running clang-format on C/C++ source in src/ directory"
 SRC=$(git ls-tree -r HEAD src | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2)
 
 clang-format -style=file -i $SRC
 
 echo "## Commiting files..."
-git commit -a -m "[no ci] apply clang-format" || true
+git commit -a -m "[no ci][no fmt] apply clang-format" || true
 
+echo "## Pushing commit to [origin HEAD:$GITHUB_HEAD_REF]"
 git push origin HEAD:$GITHUB_HEAD_REF
