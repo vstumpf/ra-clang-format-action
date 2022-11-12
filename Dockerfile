8d07e4e886a8314df9f77d4f80c6ed0666e1604b
Vincent Stumpf <vincents.995@gmail.com>
2022-11-12 01:36:59 -0800

    remove git dependency

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM ubuntu:20.04
 
 RUN apt-get update && apt-get install -y \
-    clang-format \
-    git
+    clang-format
 
 COPY LICENSE README.md /
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,7 @@
 
 set -eu
 
-# echo "## Marking pwd as safe"
-# git config --global --add safe.directory "$PWD"
-
 echo "## Running clang-format on C/C++ source in src/ directory"
-# SRC=$(git ls-tree -r HEAD src | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2)
 SRC=$(find src -type f -name "*.[ch]pp")
 
 clang-format -style=file -i $SRC
