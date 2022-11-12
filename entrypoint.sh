be99749f02bd2b9ca080d3b827954709d0a07601
Vincent Stumpf <vincents.995@gmail.com>
2022-11-11 22:01:21 -0800

    simplify git push

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,14 @@ echo "## Configuring git author..."
 git config --global user.email "clang-format@rathena.com"
 git config --global user.name "rAthena Clang Format"
 
+echo "## Using $(clang-format --version)" 
+
 echo "## Running clang-format on C/C++ source in src/ directory"
 SRC=$(git ls-tree -r HEAD src | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2)
-
-echo "## Using $(clang-format --version)" 
 
 clang-format -style=file -i $SRC
 
 echo "## Commiting files..."
 git commit -a -m "apply clang-format" || true
 
-echo "## Pushing to $BRANCH"
-git push -u origin $BRANCH
+git push
